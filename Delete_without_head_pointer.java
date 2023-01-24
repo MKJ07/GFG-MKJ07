18cb00c2d1e218d689efe27fa769534412d8dca5
MKJ07 <mahesh8479j@gmail.com>
2023-01-24 17:49:18 +0530

    Pratice Question on LinkedList

---
+++ Delete_without_head_pointer.java
@@ -0,0 +1,92 @@
+import java.util.*;
+/* */
+//{ Driver Code Starts
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+public class Delete_without_head_pointer {
+    Node head;
+    Node tail;
+
+    void printList(Node head) {
+        Node tnode = head;
+        while (tnode != null) {
+            System.out.print(tnode.data + " ");
+            tnode = tnode.next;
+        }
+        System.out.println();
+    }
+
+    void addToTheLast(Node node) {
+
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = node;
+        }
+    }
+
+    Node search_Node(Node head, int k) {
+        Node current = head;
+        while (current != null) {
+            if (current.data == k)
+                break;
+            current = current.next;
+        }
+        return current;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        while (t > 0) {
+            int n = sc.nextInt();
+            Delete_without_head_pointer llist = new Delete_without_head_pointer();
+            // int n=Integer.parseInt(br.readLine());
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) {
+                int a = sc.nextInt();
+                llist.addToTheLast(new Node(a));
+            }
+
+            int k = sc.nextInt();
+            Node del = new Node(k);
+            // Node del = llist.search_Node(llist.head, k);
+
+            Solution g = new Solution();
+            if (del != null) {
+                g.deleteNode(del);
+            }
+            llist.printList(llist.head);
+            t--;
+        }
+        sc.close();
+    }
+}
+class Solution {
+    void deleteNode(Node del) {
+        Node temp = del;
+        temp.data = del.next.data;
+        temp.next = temp.next.next;
+        // We can go by another method in this w12767e are copying the del node data
+        // to previous node and assigning the previous node to del.next address
+        /*
+        Node curr=del.next;
+        del.data=del.next.data;
+        del.next=curr.next;
+        */
+
+    }
+}
