ea8384659c7ac57d1f7efa4fe6e072ca89909e22
MKJ07 <mahesh8479j@gmail.com>
2023-07-31 00:00:16 +0530

    Praticd Problem on BST

---
+++ Check_for_BST.java
@@ -0,0 +1,151 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+/*Question:Check for BST
+Link: https://practice.geeksforgeeks.org/problems/check-for-bst/1
+Input:
+   2
+ /    \
+1      3
+Output: 1 
+Explanation: 
+The left subtree of root node contains node
+with key lesser than the root nodes key and 
+the right subtree of root node contains node 
+with key greater than the root nodes key.
+Hence, the tree is a BST.
+
+Input:
+  2
+   \
+    7
+     \
+      6
+       \
+        5
+         \
+          9
+           \
+            2
+             \
+              6
+Output: 0 
+Explanation: 
+Since the node with value 7 has right subtree 
+nodes with keys less than 7, this is not a BST.
+*/
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+public class Check_for_BST {
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Solution g = new Solution();
+        	    if(g.isBST(root))
+			        System.out.println(1);
+			    else
+			        System.out.println(0);
+                t--;
+            
+        }
+    }
+}
+class Solution
+{
+    //Function to check whether a Binary Tree is BST or not.
+    boolean isbst(Node root,int min,int max){
+        if(root==null){
+            return true;
+        }
+        if(root.data<=min || root.data>=max)
+            return false;
+        return isbst(root.left,min,root.data) && isbst(root.right,root.data,max);
+    }
+    boolean isBST(Node root)
+    {
+        // code here.
+        // int mi=I
+        return isbst(root,Integer.MIN_VALUE,Integer.MAX_VALUE);
+    }
+}
