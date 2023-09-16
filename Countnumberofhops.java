9bf6f3592a5e3c2c25a631ab1ec76580e10e8243
MKJ07 <mahesh8479j@gmail.com>
2023-09-16 19:53:58 +0530

    POTD(16/09/23)

--- Countnumberofhops.java
+++ Countnumberofhops.java
@@ -1,16 +1,9 @@
 public class Countnumberofhops {
-    // static long cal(int n){
-    //     if(n==1 || n==0){
-    //         return 1;
-    //     }
-    //     else if(n==2){
-    //         return 2;
-    //     }
-    //     else
-    //         return cal(n-3)+cal(n-2)+cal(n-1);
-    // }
+    /*This function is the optimal approach storing the value of the calculation and return if its re occur
+     * This is DP approach
+     */
     static long countWays(int n, long[] arr) {
-        int MOD = 1000000007;
+        int MOD = 1000000007;// It is use for the condition that integer does not get overflow.
         if (n <= 1) {
             return 1;
         }
@@ -34,8 +27,9 @@ public class Countnumberofhops {
         }
         else
             return countWays(n-3)+countWays(n-2)+countWays(n-1);
-         */
-        
+        */
+
+        /*This is the optimal approach having TC:O(n) and SC:O(n) */
         long[] arr = new long[n + 1];
         return countWays(n, arr);
     }
