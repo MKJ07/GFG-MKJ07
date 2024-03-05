3f31285186f664cb020bd27d36d154a0285c8953
MKJ07 <mahesh8479j@gmail.com>
2024-03-05 22:50:38 +0530

    POTD(05/03/24)

--- MaximumIndex.java
+++ MaximumIndex.java
@@ -1,6 +1,21 @@
 public class MaximumIndex {
     static int maxIndexDiff(int a[], int n) {
-        /*This is the brute force approach having TC: O(n^2) and SC: O(1) */
+        /*This is a better approach having TC: O(~n) and SC: O(1) */
+        int max=Integer.MIN_VALUE;
+        int left=0,right=n-1;
+        while(left<=right){
+            if(a[left]<=a[right]){
+                int temp=right-left;
+                max=Math.max(max,temp);
+                left++;
+                right=n-1;
+            }
+            else{
+                right--;
+            }
+        }
+
+        /*This is the brute force approach having TC: O(n^2) and SC: O(1)
         int max=Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
@@ -9,7 +24,7 @@ public class MaximumIndex {
                     max=Math.max(max,temp);
                 }
             }
-        }
+        }*/
         return max;
     }
     public static void main(String[] args) {
