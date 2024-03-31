d74c74b77d4ba3d84bf11f4f1b6cd78476bb12aa
MKJ07 <mahesh8479j@gmail.com>
2024-03-31 22:12:25 +0530

    Deleting a Node from List

--- Delete_without_head_pointer.java
+++ Delete_without_head_pointer.java
@@ -1,4 +1,4 @@
-import java.util.*;
+// import java.util.*;
 /* */
 //{ Driver Code Starts
 class Node {
@@ -8,6 +8,21 @@ class Node {
     Node(int d) {
         data = d;
         next = null;
+    }
+}
+class Solution {
+    void deleteNode(Node del) {
+        Node temp = del;
+        temp.data = del.next.data;
+        temp.next = temp.next.next;
+        // We can go by another method in this w12767e are copying the del node data
+        // to previous node and assigning the previous node to del.next address
+        /*
+        Node curr=del.next;
+        del.data=del.next.data;
+        del.next=curr.next;
+        */
+
     }
 }
 
@@ -46,47 +61,52 @@ public class Delete_without_head_pointer {
     }
 
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
+    Delete_without_head_pointer llist = new Delete_without_head_pointer();
+    Node head = new Node(1); // First element of the linked list
+    llist.addToTheLast(head);
+    // Add remaining elements to the linked list
+    llist.addToTheLast(new Node(2));
+    llist.addToTheLast(new Node(3));
+    llist.addToTheLast(new Node(4));
+    llist.addToTheLast(new Node(5));
+    
+    int k = 3; // Value of the node to be deleted
+    
+    Node del = new Node(k);
+    // Node del = llist.search_Node(llist.head, k);
 
-        int t = sc.nextInt();
-        while (t > 0) {
-            int n = sc.nextInt();
-            Delete_without_head_pointer llist = new Delete_without_head_pointer();
-            // int n=Integer.parseInt(br.readLine());
-            int a1 = sc.nextInt();
-            Node head = new Node(a1);
-            llist.addToTheLast(head);
-            for (int i = 1; i < n; i++) {
-                int a = sc.nextInt();
-                llist.addToTheLast(new Node(a));
-            }
-
-            int k = sc.nextInt();
-            Node del = new Node(k);
-            // Node del = llist.search_Node(llist.head, k);
-
-            Solution g = new Solution();
-            if (del != null) {
-                g.deleteNode(del);
-            }
-            llist.printList(llist.head);
-            t--;
-        }
-        sc.close();
+    Solution g = new Solution();
+    if (del != null) {
+        g.deleteNode(del);
     }
-}
-class Solution {
-    void deleteNode(Node del) {
-        Node temp = del;
-        temp.data = del.next.data;
-        temp.next = temp.next.next;
-        // We can go by another method in this w12767e are copying the del node data
-        // to previous node and assigning the previous node to del.next address
-        /*
-        Node curr=del.next;
-        del.data=del.next.data;
-        del.next=curr.next;
-        */
+    llist.printList(llist.head);
 
+        // Scanner sc = new Scanner(System.in);
+
+        // int t = sc.nextInt();
+        // while (t > 0) {
+        //     int n = sc.nextInt();
+        //     Delete_without_head_pointer llist = new Delete_without_head_pointer();
+        //     // int n=Integer.parseInt(br.readLine());
+        //     int a1 = sc.nextInt();
+        //     Node head = new Node(a1);
+        //     llist.addToTheLast(head);
+        //     for (int i = 1; i < n; i++) {
+        //         int a = sc.nextInt();
+        //         llist.addToTheLast(new Node(a));
+        //     }
+
+        //     int k = sc.nextInt();
+        //     Node del = new Node(k);
+        //     // Node del = llist.search_Node(llist.head, k);
+
+        //     Solution g = new Solution();
+        //     if (del != null) {
+        //         g.deleteNode(del);
+        //     }
+        //     llist.printList(llist.head);
+        //     t--;
+        // }
+        // sc.close();
     }
 }
