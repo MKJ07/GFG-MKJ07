f79ad14a876e56dbd7cfe301f66f94bc32d72815
MKJ07 <mahesh8479j@gmail.com>
2024-04-16 23:11:00 +0530

    POTD(16/04/24)

--- MinimizetheDifference.java
+++ MinimizetheDifference.java
@@ -1,9 +1,37 @@
 import java.util.HashMap;
 import java.util.TreeSet;
+/*Question: Minimize the Difference. 
+Link: https://www.geeksforgeeks.org/problems/minimize-the-difference/1
+
+Example 1:
+Input:
+n = 5
+k = 3
+arr = {1, 2, 3, 4, 5}
+Output: 
+1
+Explanation: 
+We can remove first subarray of length 3(i.e. {1, 2, 3}) then remaining array will be {4,5} and the difference of maximum and minimum element will be 1 i.e 5 - 4 = 1
+
+Example 2:
+Input:
+n = 6
+k = 3
+arr = {2, 3, 1, 4, 6, 7}
+Output: 
+2
+Explanation:
+If we remove the subarray {2,3,1} then remaining array will be {4,6,7} and the difference  = 7-4 = 3
+If we remove the subarray {3,1,4} then remaining array will be {2,6,7} and the difference  = 7-2 = 5
+If we remove the subarray {1,4,6} then remaining array will be {2,3,7} and the difference  = 7-2 = 5
+If we remove the subarray {4,6,7} then remaining array will be {2,3,1} and the difference  = 3-1 = 2
+So the answer will be min(3,5,5,2) = 2
+*/
 
 public class MinimizetheDifference {
     public static int minimizeDifference(int n, int k, int[] arr) {
-        // Set<Integer> st=new HashSet<>();
+        /*This appraoch has TC: O(n^2) {when using set} & O(nlogn) {when using TreeSet} and SC: O(n) */
+        // Set<Integer> st=new HashSet<>(); {we can also use set}
         TreeSet<Integer> st=new TreeSet<>();
         for(int i:arr){
             st.add(i);
@@ -25,7 +53,7 @@ public class MinimizetheDifference {
             if(h.get(ele)==0)
                 st.remove(ele);
         }
-        // int ans=Collections.max(st)-Collections.min(st);
+        // int ans=Collections.max(st)-Collections.min(st); {this should be considered when using set}
         int ans=st.last()-st.first();
         while(e<n){
             int ele1=arr[s];
