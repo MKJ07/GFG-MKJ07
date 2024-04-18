402d28ee48bfbf1934ed8878c90914ba395d5ef1
MKJ07 <mahesh8479j@gmail.com>
2024-04-18 23:45:17 +0530

    POTD(18/04/24)

--- TwoRepeatedElements.java
+++ TwoRepeatedElements.java
@@ -1,3 +1,25 @@
+/*Question: Two Repeated Elements.
+Link: https://www.geeksforgeeks.org/problems/two-repeated-elements-1587115621/1
+
+Example 1:
+Input:
+n = 4
+arr[] = {1,2,1,3,4,3}
+Output: 
+1 3
+Explanation: 
+In the given array, 1 and 3 are repeated two times and as 1's second appearance occurs before 2's second appearance, so the output should be 1 3.
+
+Example 2:
+Input:
+n = 2
+arr[] = {1,2,2,1}
+Output: 
+2 1
+Explanation: 
+In the given array, 1 and 2 are repeated two times and second occurence of 2 comes before 1. So the output is 2 1.
+*/
+
 public class TwoRepeatedElements {
     public int[] twoRepeated(int arr[], int n)
     {
@@ -16,28 +38,29 @@ public class TwoRepeatedElements {
         }
         return res;
         
-        /*This is the brute force approach having TC: O(n) and SC: O(n) {n: no of elements in the array} */
-        // HashMap<Integer,Integer> h1=new HashMap<>();
-        // for(int i:arr){
-        //     if(h1.containsKey(i))
-        //         h1.put(i,h1.get(i)+1);
-        //     else
-        //         h1.put(i,1);
-        // }
-        // int[] visit=new int[n+1];
-        // int[] res=new int[2];
-        // int idx=0;
-        // for(int i=0;i<n+2;i++){
-        //     if(h1.get(arr[i])>1){
-        //         if(visit[arr[i]]>0){
-        //             res[idx]=arr[i];
-        //             idx++;
-        //         }
-        //         else
-        //             visit[arr[i]]++;
-        //     }
-        // }
-        // return res;
+        /*This is the brute force approach having TC: O(n) and SC: O(n) {n: no of elements in the array} 
+        HashMap<Integer,Integer> h1=new HashMap<>();
+        for(int i:arr){
+            if(h1.containsKey(i))
+                h1.put(i,h1.get(i)+1);
+            else
+                h1.put(i,1);
+        }
+        int[] visit=new int[n+1];
+        int[] res=new int[2];
+        int idx=0;
+        for(int i=0;i<n+2;i++){
+            if(h1.get(arr[i])>1){
+                if(visit[arr[i]]>0){
+                    res[idx]=arr[i];
+                    idx++;
+                }
+                else
+                    visit[arr[i]]++;
+            }
+        }
+        return res; 
+        */
     }
     public static void main(String[] args) {
         int n = 4;
